1b8357c77ba575d5d1f4742280d63b042500e2de
Yuiheart <151250023@smail.nju.edu.cn>
2019-05-08 15:08:02 +0800

    Revert "Update bank.java"
    This reverts commit a7b4e51da16b7008995b2ba40ec1a154a4c110fa.

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -16,34 +16,13 @@ public class Bank {
 
     //返回固定格式的客户列表,格式参考测试用例
     public String customerSummary() {
-        String summary = "Customer Summary\n";
-        int NumOfCustomers = this.customers.size();
-        if(NumOfCustomers == 1){
-            Customer temp = customers.get(0);
-            summary = summary + " - " + temp.getName() + " " + "(" + temp.getNumberOfAccounts() + " accounts)";
-        }
-        if(NumOfCustomers >= 2) {
-            for (int i = 0; i < NumOfCustomers - 1; i++) {
-                Customer temp = customers.get(i);
-                summary = summary + " - " + temp.getName() + " " + "(" + temp.getNumberOfAccounts() + " accounts)";
-                summary = summary + "\n";
-            }
-            Customer FinalCustomer = customers.get(customers.size() - 1);
-            summary = summary + " - " + FinalCustomer.getName() + " " + "(" + FinalCustomer.getNumberOfAccounts() + " accounts)";
-        }
+        String summary = "";
         return summary;
     }
 
     //返回支付所有客户的利息总和
     public double totalInterestPaid() {
         double total = 0;
-        if(this.customers.size() != 0){
-            for(int i = 0; i < this.customers.size(); i++){
-                Customer temp = customers.get(i);
-                double InterestOfCustomer = temp.totalInterestEarned();
-                total += InterestOfCustomer;
-            }
-        }
         return total;
     }
 
