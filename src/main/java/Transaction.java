45803b599fc358f85b1e3817166e2ec82f28d501
rex <xuzhuoer.rex@gmail.com>
2019-05-08 14:34:12 +0800

    Init project

---
+++ src/main/java/Transaction.java
@@ -0,0 +1,14 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class Transaction {
+    public final double amount;
+
+    private Date transactionDate;
+
+    public Transaction(double amount) {
+        this.amount = amount;
+        this.transactionDate = DateProvider.getInstance().now();
+    }
+
+}
